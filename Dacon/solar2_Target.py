2d522264773939a0d350cc02abecb81f6651899d
lynhyul <lynhyul@naver.com>
2021-01-19 15:17:46 +0900

    c

---
+++ Dacon/solar2_Target.py
@@ -0,0 +1,147 @@
+import pandas as pd
+import numpy as np
+import os
+import glob
+import tensorflow as tf
+import warnings
+warnings.filterwarnings("ignore")
+
+train = pd.read_csv('../data/csv/train/train.csv',index_col=False)
+# test = pd.read_csv('../data/csv/test/test.csv',index_col=False)
+# modelpath = '../data/modelcheckpoint/태양광_{epoch:02d}-{val_loss:.4f}.hdf5'
+df_test = []
+for i in range(81) :
+    filepath = '../data/csv/test/{}.csv'.format(i)
+    # globals()['test{}'.format(i)] = pd.read_csv(filepath,index_col=False)
+    df = pd.read_csv(filepath,index_col=False)
+    df = df[['Hour', 'TARGET', 'DHI', 'DNI', 'WS', 'RH', 'T']] 
+    df_test.append(df)
+X_test = pd.concat(df_test)
+print(X_test)
+
+temp = train.copy()
+temp = temp[['Hour', 'TARGET', 'DHI', 'DNI', 'WS', 'RH', 'T']]      
+temp['Target1'] = temp['TARGET'].shift(-48).fillna(method='ffill')
+temp['Target2'] = temp['TARGET'].shift(-48*2).fillna(method='ffill')
+temp = temp.dropna()
+
+quantiles = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+# train_x = train.iloc[:,3:-1]
+print(temp.tail())
+
+x = temp.iloc[:-96,:-2]
+y = temp.iloc[:-96,-2:]
+
+x = x.to_numpy()
+y = y.to_numpy()
+
+pred = X_test.to_numpy()
+# print(pred.shape)   #27216,7
+
+# print(x.shape)  #(52464, 7)
+
+x = x.reshape(1093,48,7)
+y = y.reshape(1093,48*2)
+pred = pred.reshape(567,48,7)
+# x = x.reshape(x.shape[0]/48,48,2)
+# y = y.reshape(y.shape[0]/48,48,2)
+
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2, shuffle = True, random_state=101)
+# x_train, x_val, y_train, y_val = train_test_split(x_train,y_train,train_size = 0.8)
+
+print(x_train.shape) 
+
+# x_train = x_train.reshape(x_train.shape[0],x_train.shape[1]*x_train.shape[2])
+# x_test = x_test.reshape(x_test.shape[0],x_test.shape[1]*x_test.shape[2])
+# x_val = x_val.reshape(x_val.shape[0],x_val.shape[1]*x_val.shape[2])
+
+
+
+# print(x_train.shape)
+
+# from sklearn.preprocessing import MinMaxScaler
+# scaler = MinMaxScaler()
+# scaler.fit(x_train)
+# x_train = scaler.transform(x_train)
+# x_test = scaler.transform(x_test)
+# x_val = scaler.transform(x_val)
+
+
+
+# x_train = x_train.reshape(x_train.shape[0],48,7)
+# x_test = x_test.reshape(x_test.shape[0],48,7)
+# x_val = x_val.reshape(x_val.shape[0],48,7)
+# pred = pred.reshape(567,48,7)
+
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Input, LSTM, Dropout, Conv1D, Reshape, Flatten
+inputs = Input(shape=(48,7))
+dense1 = Conv1D(512, 2, padding='same')(inputs)
+dense1 = Flatten()(dense1)
+dense1 = Dense(128)(dense1)
+dense1 = Dense(64)(dense1)
+dense1 = Dense(32)(dense1)
+dense1 = Dense(16)(dense1)
+outputs = Dense(48*2)(dense1)
+
+model = Model(inputs=inputs, outputs=outputs)
+
+model.summary()
+
+# model = Model(input1, output1)
+
+# #3. compile fit
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+modelpath = '../data/modelcheckpoint/태양광_{epoch:02d}-{val_loss:.4f}.hdf5'
+es = EarlyStopping(monitor='val_loss', patience=40)
+cp = ModelCheckpoint(filepath=modelpath, monitor='val_loss',save_best_only=True,mode='auto')
+
+lr = ReduceLROnPlateau(monitor='val_loss',patience=20, factor=0.5, verbose=1)
+#n번까지 참았는데도 개선이없으면 50퍼센트 감축시키겠다.
+
+model.compile(loss = 'mse', optimizer='adam', metrics=['mae'])
+# model.fit(x_train,y_train,epochs=300, batch_size=120, validation_split=0.2,
+# callbacks = [es,cp,lr])
+
+# loss = model.evaluate(x_test,y_test, batch_size=1)
+# print("loss : ",loss)
+
+
+
+d = []  
+for l in range(9):
+    cp = ModelCheckpoint(filepath = '../data/modelcheckpoint/dacon%d.hdf5'%l,monitor='val_loss', save_best_only=True)
+    model.fit(x,y,epochs= 300, validation_split=0.2, batch_size =120, callbacks = [es,cp,lr])
+    y_pred = model.predict(pred)
+    y_pred = y_pred.reshape(27216,2)
+    d.append(y_pred)
+    
+
+df_sub = pd.read_csv('../data/csv/sample_submission.csv', index_col = 0, header = 0)
+
+# submit 파일에 데이터들 덮어 씌우기!!
+for i in range(81):
+    for j in range(2):
+        for k in range(48):
+            df = pd.DataFrame(d[i,j,k])
+            for l in range(9):
+                df_sub.iloc[[i*96+j*48+k],[l]] = df.quantile(q = ((l+1)/10.),axis = 0)[0]
+
+df_sub.to_csv('../data/csv/submit.csv')
+
+
+
+
+# for b in range(81) :
+#     globals()['result{}'.format(b)] = (model.predict(globals()['pred{}'.format(b)])).reshape(96,6)
+#     globals()['result{}'.format(b)] = (model.predict(globals()['pred{}'.format(b)])).reshape(96,6)
+#     # np.savetxt('../data/csv/태양광{}test.csv'.format(b),globals()['result{}'.format(b)],delimiter=",")
+#     # globals()['result{}'.format(b)] = np.around((globals()['pred{}'.format(b)]),3)
+
+# # print("\n",result78)   # 96,6
+# '''
+# loss :  [25677.662109375, 0.010015263222157955]
--- Dacon/solar_data1.py
+++ Dacon/solar_data1.py
@@ -2,13 +2,8 @@ import pandas as pd
 import numpy as np
 import os
 import glob
-import random
 import tensorflow as tf
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import tensorflow.keras.backend as K
-import tensorflow.keras.layers as L
-import tensorflow.keras.models as M
+
 
 
 train = pd.read_csv('../data/csv/train/train.csv',index_col=False)
@@ -28,17 +23,21 @@ train_x = train.iloc[:,3:]
 0        0    0  1.5  69.08 -12     0.0
 1        0    0  1.5  69.06 -12     0.0
 '''
-x_data = train_x.to_numpy()
+data = train_x.to_numpy()
+data = data.reshape(1095,48,6)
+print(data.shape) # 1095,48,6
+
 for a in range(81) :
     globals()['pred{}'.format(a)] = globals()['test_{}'.format(a)].to_numpy()
 # print(pred80.shape) # 336,6
 
-def split_xy2(dataset,time_steps,y_column) :
-    x, y = list(),list()
-    for i in range(len(dataset)) :
-        x_end_number = i+ time_steps
-        y_end_number = x_end_number + y_column
-        if y_end_number > len(dataset) :
+def split_xy(dataset, timesteps_x, timesteps_y):
+    x, y = list(), list()
+    
+    for i in range(len(data)):
+        x_end_number = i + timesteps_x
+        y_end_number = x_end_number + timesteps_y
+        if y_end_number > len(dataset):
             break
         tmp_x = dataset[i : x_end_number]
         tmp_y = dataset[x_end_number : y_end_number]
@@ -46,12 +45,19 @@ def split_xy2(dataset,time_steps,y_column) :
         y.append(tmp_y)
     return np.array(x), np.array(y)
 
-time_steps = 336
-y_column = 96
-x, y = split_xy2(x_data, time_steps, y_column)
+timesteps_x = 7
+timesteps_y = 2
+
+x, y = split_xy(data, timesteps_x, timesteps_y)
+print(x.shape, y.shape) # (1087, 7, 48, 6) (1087, 2, 48, 6)
+
+y = y.reshape(1087, 2*48*6)
+
 print(x.shape)
-# for b in range(81) :
-#     globals()['pred{}'.format(b)]= (globals()['pred{}'.format(b)]).reshape(1,336,6)
+for b in range(81) :
+    globals()['pred{}'.format(b)]= (globals()['pred{}'.format(b)]).reshape(1,336,6)
+print(x.shape)      # 695, 7, 48, 6
+print(y.shape)      # 695, 7, 48, 6
 
 # print(x.shape) # 52129, 336, 6
 # print(y.shape) # 52129, 96, 6
@@ -59,94 +65,81 @@ print(x.shape)
 # # print(pred0.shape)  # 1,336,6
 
 
-# from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 
-# x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2, shuffle = True, random_state=101)
-# x_train, x_val, y_train, y_val = train_test_split(x_train,y_train,train_size = 0.8)
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2, shuffle = True, random_state=101)
+x_train, x_val, y_train, y_val = train_test_split(x_train,y_train,train_size = 0.8)
 
-# print(x_train.shape) # (33633, 6, 6)
+print(x_train.shape) # (33633, 6, 6)
 
-# x_train = x_train.reshape(x_train.shape[0],x_train.shape[1]*x_train.shape[2])
-# x_test = x_test.reshape(x_test.shape[0],x_test.shape[1]*x_test.shape[2])
-# x_val = x_val.reshape(x_val.shape[0],x_val.shape[1]*x_val.shape[2])
-# for b in range(81) :
-#     globals()['pred{}'.format(b)]= (globals()['pred{}'.format(b)]).reshape(1,336*6)
+x_train = x_train.reshape(x_train.shape[0],x_train.shape[1]*x_train.shape[2]*x_train.shape[3])
+x_test = x_test.reshape(x_test.shape[0],x_test.shape[1]*x_test.shape[2]*x_test.shape[3])
+x_val = x_val.reshape(x_val.shape[0],x_val.shape[1]*x_val.shape[2]*x_val.shape[3])
+for b in range(81) :
+    globals()['pred{}'.format(b)]= (globals()['pred{}'.format(b)]).reshape(1,336*6)
 
-# print(x_train.shape)
+print(x_train.shape)
 
-# from sklearn.preprocessing import MinMaxScaler
-# scaler = MinMaxScaler()
-# scaler.fit(x_train)
-# x_train = scaler.transform(x_train)
-# x_test = scaler.transform(x_test)
-# x_val = scaler.transform(x_val)
-# for b in range(81) :
-#     globals()['pred{}'.format(b)]= scaler.transform(globals()['pred{}'.format(b)])
-
-
-# x_train = x_train.reshape(x_train.shape[0],336,6)
-# x_test = x_test.reshape(x_test.shape[0],336,6)
-# x_val = x_val.reshape(x_val.shape[0],336,6)
-# for b in range(81) :
-#     globals()['pred{}'.format(b)]= (globals()['pred{}'.format(b)]).reshape(1,336,6)
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+x_val = scaler.transform(x_val)
+for b in range(81) :
+    globals()['pred{}'.format(b)]= scaler.transform(globals()['pred{}'.format(b)])
 
 
-# from tensorflow.keras.models import Sequential, Model
-# from tensorflow.keras.layers import Dense, LSTM, Input, Conv1D, Dropout, Flatten,MaxPooling1D
+x_train = x_train.reshape(x_train.shape[0],7,48,6)
+x_test = x_test.reshape(x_test.shape[0],7,48,6)
+x_val = x_val.reshape(x_val.shape[0],7,48,6)
+for b in range(81) :
+    globals()['pred{}'.format(b)]= (globals()['pred{}'.format(b)]).reshape(1,336,1,6)
 
 
-# model = Sequential()
-# model.add(Conv1D(filters = 20, kernel_size = 2, input_shape=(336,6)))
-# # model.add(MaxPooling1D(pool_size=2))
-# # model.add(Dropout(0.2))
-# model.add(Flatten())
-# model.add(Dense(512, activation= 'relu'))
-# model.add(Dense(256, activation= 'relu'))
-# model.add(Dense(96*6))
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Input, LSTM, Dropout, Conv2D, Reshape, Flatten
+inputs = Input(shape=(7,48,6))
+dense1 = Conv2D(512, 2, padding='same')(inputs)
+dense1 = Flatten()(dense1)
+dense1 = Dense(32)(dense1)
+dense1 = Dense(16)(dense1)
+outputs = Dense(2*48*6)(dense1)
+
+model = Model(inputs=inputs, outputs=outputs)
+
+model.summary()
+
+# model = Model(input1, output1)
+
+# #3. compile fit
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+modelpath = '../data/modelcheckpoint/태양광_{epoch:02d}-{val_loss:.4f}.hdf5'
+early_stopping = EarlyStopping(monitor='val_loss', patience=40)
+cp = ModelCheckpoint(filepath=modelpath, monitor='val_loss',save_best_only=True,mode='auto')
+
+reduce_lr = ReduceLROnPlateau(monitor='val_loss',patience=20, factor=0.5, verbose=1)
+#n번까지 참았는데도 개선이없으면 50퍼센트 감축시키겠다.
+
+model.compile(loss = 'mse', optimizer='adam', metrics=['mae'])
+model.fit(x_train,y_train,epochs=300, batch_size=120, validation_data=(x_val,y_val),
+callbacks = [early_stopping,cp,reduce_lr])
 
 
-# model.summary()
-
-# # model = Model(input1, output1)
-
-# # #3. compile fit
-# from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
-# modelpath = '../data/modelcheckpoint/태양광_{epoch:02d}-{val_loss:.4f}.hdf5'
-# early_stopping = EarlyStopping(monitor='val_loss', patience=15)
-# cp = ModelCheckpoint(filepath=modelpath, monitor='val_loss',save_best_only=True,mode='auto')
-
-# reduce_lr = ReduceLROnPlateau(monitor='val_loss',patience=8, factor=0.5, verbose=1)
-# #n번까지 참았는데도 개선이없으면 50퍼센트 감축시키겠다.
-
-# model.compile(loss = 'mse', optimizer='adam', metrics=['mae'])
-# model.fit(x_train,y_train,epochs=5, batch_size=120, validation_data=(x_val,y_val),
-# callbacks = [early_stopping,cp,reduce_lr])
-
-# # from tensorflow.keras.callbacks import EarlyStopping
-# # early_stopping = EarlyStopping(monitor='loss', patience= 5, mode = 'auto')
-
-# # model.compile(loss = 'mse', optimizer='adam', metrics=['acc'])
-# # model.fit(x_train,y_train, epochs=100, batch_size=2, validation_data=(x_val, y_val),  
-# #                                      callbacks = early_stopping)
-
-# #4. evaluate , predict
-# # y_train = y.train.reshape(y_train.shape[0],96,6)
-# # y_test = y.test.reshape(y_test.shape[0],96,6)
-
-# loss = model.evaluate(x_test,y_test, batch_size=1)
-# print("loss : ",loss)
-
-# # y = y.reshape(y.shape[0],96,6)
-# # for b in range(81) :
-# #     globals()['pred{}'.format(b)]= (globals()['pred{}'.format(b)]).reshape(1,336,6)
-# result = model.predict(pred0)
-# result = result.reshape(-1,96,6)
-# print("\n",result)
-# '''
-# loss :  [25677.662109375, 0.010015263222157955]
-# '''
+loss = model.evaluate(x_test,y_test, batch_size=1)
+print("loss : ",loss)
 
 
-# # # x_pred = x_pred.reshape(1,5,1)
-# # # y_predict = model.predict(x_pred)
-# # # print("y_predcit: ",y_predict)
+for b in range(81) :
+    globals()['result{}'.format(b)] = (model.predict(globals()['pred{}'.format(b)])).reshape(96,6)
+    globals()['result{}'.format(b)] = (model.predict(globals()['pred{}'.format(b)])).reshape(96,6)
+    # np.savetxt('../data/csv/태양광{}test.csv'.format(b),globals()['result{}'.format(b)],delimiter=",")
+    # globals()['result{}'.format(b)] = np.around((globals()['pred{}'.format(b)]),3)
+
+# print("\n",result78)   # 96,6
+'''
+loss :  [25677.662109375, 0.010015263222157955]
+'''
+
+
