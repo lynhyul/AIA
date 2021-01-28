912826b70f011ba6f356cd2fe4416e97f5385fa4
lynhyul <lynhyul@naver.com>
2021-01-28 17:38:22 +0900

    튜닝실패?..

--- ml/m13_randomSearch2_1_iris.py
+++ ml/m13_randomSearch2_1_iris.py
@@ -53,11 +53,16 @@ date_time = date_now1.strftime("%m월%d일_%H시%M분%S초")
 print("start time: ",date_time)
 
 parameters = [
-    {"n_estimators" : [100,200,300]},
-    {'max_depth' : [-1,2,4,6,8,10]},
-    {'min_samples_leaf' : [3,5,7,10,12,14]},
-    {'min_samples_split' : [2,3,5,10,12,14]},
-    {'n_jobs' : [-1,2,4]}
+    {"n_estimators" : [100,200,300,400],
+    'max_depth' : [-1,2,4,6,8,10],
+    'min_samples_leaf' : [3,5,7,10,12],
+    'min_samples_split' : [2,3,5,10,12],
+    'n_jobs' : [-1,6,8,10]}
+    # {"n_estimators" : [100,200,300]},
+    # {'max_depth' : [-1,2,4,6,8,10]},
+    # {'min_samples_leaf' : [3,5,7,10,12,14]},
+    # {'min_samples_split' : [2,3,5,10,12,14]},
+    # {'n_jobs' : [-1,2,4]}
 ]
 
 
@@ -89,10 +94,19 @@ griSearch time
 best score :  0.9666666666666667
 걸린시간 :  0:00:18.401929
 
+튜닝 적용 전
 start time:  01월28일_17시06분42초
 best parameter :  RandomForestClassifier(min_samples_split=10)
 best score :  0.9666666666666667
 End time:  01월28일_17시06분54초
 걸린시간 :  0:00:11.658986
+
+튜닝 적용 후 (구린가보다..)
+start time:  01월28일_17시32분11초
+best parameter :  RandomForestClassifier(max_depth=8, min_samples_leaf=5, min_samples_split=5,
+                       n_estimators=400, n_jobs=6)
+best score :  0.9333333333333333
+End time:  01월28일_17시32분44초
+걸린시간 :  0:00:32.834127
 
 '''
