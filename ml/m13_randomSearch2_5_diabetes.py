a640195f9b79a9868bb86596e6fe525774b827f1
lynhyul <lynhyul@naver.com>
2021-01-28 16:47:59 +0900

    randomize

--- ml/m13_randomSearch2_5_diabetes.py
+++ ml/m13_randomSearch2_5_diabetes.py
@@ -1,19 +1,15 @@
 #다중분류
-# Model = RandomForestClassifier
+# MinMax scaler
 
 import numpy as np
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split, KFold, cross_val_score,GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from sklearn.metrics import accuracy_score
-
+from sklearn.metrics import accuracy_score, r2_score
 from sklearn.svm import LinearSVC,SVC
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-
-import datetime
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+import pandas as pd
 
 # from sklearn.neighbors import KNeighborsClassifier
 # from sklearn.ensemble import RandomForestClassifier
@@ -25,9 +21,14 @@ warnings.filterwarnings('ignore')
 #1. data
 #x, y = load_iris(return_X_y=True)
 
-dataset = load_iris()
-x = dataset.data
-y = dataset.target
+dataset = load_diabetes()
+
+
+x = dataset.iloc[:,:-1]
+y = dataset.iloc[:,-1]
+
+dataset = dataset.to_numpy()
+
 
 print(x.shape)      # (150,4)
 print(y.shape)      # (150,)
@@ -40,22 +41,18 @@ print(y)
 print(x.shape)  # (150,4)
 print(y.shape)  # (150,3)
 
+import datetime
+
 x_train, x_test, y_train, y_test = train_test_split(x,y,random_state=110,
 shuffle = True, train_size = 0.8 )
 
 kfold = KFold(n_splits=5, shuffle=True)
 
-
-date_now = datetime.datetime.now()
-date_time = date_now.strftime("%m월%d일_%H시%M분%S초")
+date_now1 = datetime.datetime.now()
+date_time = date_now1.strftime("%m월%d일_%H시%M분%S초")
 print("start time: ",date_time)
 
 parameters = [
-    # {"n_estimators" : [300],
-    # 'max_depth' : [-1],
-    # 'min_samples_leaf' : [-1,2,14],
-    # 'min_samples_split' : [-1,2,14],
-    # 'n_jobs' :[-1]}
     {"n_estimators" : [100,200,300]},
     {'max_depth' : [-1,2,4,6,8,10]},
     {'min_samples_leaf' : [3,5,7,10,12,14]},
@@ -63,34 +60,33 @@ parameters = [
     {'n_jobs' : [-1,2,4]}
 ]
 
+
 #2. modeling
 
 # model = SVC()
-model = GridSearchCV(RandomForestClassifier(), parameters, cv=kfold)
-
+# model = GridSearchCV(SVC(), parameters, cv=kfold)
+model = RandomizedSearchCV(RandomForestRegressor(), parameters, cv=kfold)
 #3. fit
 model.fit(x_train, y_train)
 
 #4. evoluate, predict
 print("best parameter : ", model.best_estimator_)
-
+# best parameter :  SVC(C=1, kernel='linear')
 
 
 y_pred = model.predict(x_test) # grid serach
-print("best score : ",accuracy_score(y_test, y_pred))
+print("best score : ",r2_score(y_test, y_pred))
 # 1.0
 
-date_now = datetime.datetime.now()
-date_time = date_now.strftime("%m월%d일_%H시%M분%S초")
+date_now2 = datetime.datetime.now()
+date_time = date_now2.strftime("%m월%d일_%H시%M분%S초")
 print("End time: ",date_time)
-
+print("걸린시간 : ",(date_now2-date_now1))
 
 
 '''
 Deep learning
 loss :  [0.01415738184005022, 1.0]
-
-best parameter :  RandomForestClassifier()
-best score :  0.9666666666666667
-
- '''
+best parameter :  SVC(C=100, gamma=0.001)
+best score :  1.0
+'''
