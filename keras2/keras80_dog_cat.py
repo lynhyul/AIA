2ad757b9453cc24aad1593792a39c985a2ceaf0a
lynhyul <lynhyul@naver.com>
2021-03-03 11:16:21 +0900

    피티에스디

--- keras2/keras80_dog_cat.py
+++ keras2/keras80_dog_cat.py
@@ -2,3 +2,66 @@
 # 강아지, 고양이, 라이언, 슈트 욜케 넣을것
 # 파일명 : dog1.jpg, cat1.jpg, lion1.jpg, suit1.jpg
 
+from tensorflow.keras.applications import VGG16
+import matplotlib.pyplot as plt
+from keras.preprocessing.image import load_img, img_to_array
+import numpy as np
+
+img_dog = load_img('../data/image/vgg/dog1.jfif', target_size=(224,224))
+img_cat = load_img('../data/image/vgg/cat1.jpg', target_size=(224,224))
+img_lion = load_img('../data/image/vgg/lion1.jfif', target_size=(224,224))
+img_suit = load_img('../data/image/vgg/suit1.jfif', target_size=(224,224))
+
+# plt.imshow(img_dog)
+# plt.show()
+
+arr_dog = img_to_array(img_dog)
+arr_cat = img_to_array(img_cat)
+arr_lion = img_to_array(img_lion)
+arr_suit = img_to_array(img_suit)
+
+arr_input = np.stack([arr_dog,arr_cat,arr_lion,arr_suit])
+
+print(arr_dog[1][1])
+print(type(arr_dog))
+from tensorflow.keras.applications.vgg16 import preprocess_input
+
+arr_dog = preprocess_input(arr_dog)
+arr_cat = preprocess_input(arr_cat)
+arr_lion = preprocess_input(arr_lion)
+arr_suit = preprocess_input(arr_suit)
+
+print(arr_dog[1][1])
+print(type(arr_dog))
+
+
+# Model
+model = VGG16()
+results = model.predict(arr_input)
+
+print(results)
+print('result.shape : ', results.shape)
+
+
+#Check results
+from tensorflow.keras.applications.vgg16 import decode_predictions
+
+decode_results = decode_predictions(results)
+print("results[0] : ",decode_results[0])
+print("========================================")
+print("results[1] : ",decode_results[1])
+print("========================================")
+print("results[2] : ",decode_results[2])
+print("========================================")
+print("results[3] : ",decode_results[3])
+print("========================================")
+'''
+results[0] :  [('n02110185', 'Siberian_husky', 0.5978164), ('n02109961', 'Eskimo_dog', 0.3819922), ('n02110063', 'malamute', 0.0072567468), ('n02113186', 'Cardigan', 0.003448518), ('n02114548', 'white_wolf', 0.0025010263)]
+========================================
+results[1] :  [('n02124075', 'Egyptian_cat', 0.45797604), ('n02123045', 'tabby', 0.4130136), ('n02123159', 'tiger_cat', 0.1273834), ('n02127052', 'lynx', 0.00030834926), ('n04040759', 'radiator', 0.00024303942)]
+========================================
+results[2] :  [('n03532672', 'hook', 0.35332686), ('n02951585', 'can_opener', 0.23019917), ('n04579432', 'whistle', 0.19786789), ('n04317175', 'stethoscope', 0.05041925), ('n04127249', 'safety_pin', 0.042389903)]
+========================================
+results[3] :  [('n04350905', 'suit', 0.47582898), ('n02883205', 'bow_tie', 0.28350362), ('n04591157', 'Windsor_tie', 0.19237481), ('n02963159', 'cardigan', 0.026376557), ('n04599235', 'wool', 0.011495415)]
+========================================
+'''
---
+++ keras2/keras81_male_female.py
@@ -0,0 +1,134 @@
+# vgg16으로 만들어봥!!
+
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import VGG16
+from keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.layers import Dense, Flatten,BatchNormalization, Activation, GlobalAveragePooling2D
+
+from keras.utils import np_utils
+
+import os, glob, numpy as np
+from PIL import Image
+from numpy import asarray
+from tensorflow.keras.models import Model, Sequential,load_model
+from tensorflow.keras.layers import GlobalAveragePooling2D, Activation
+
+import scipy.signal as signal
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+
+
+#모델 불러오기, 원핫인코딩은 덤으로!
+
+img_dir = '../data/image/sex/'
+categories = ['0', '1']
+np_classes = len(categories)
+
+image_w = 255
+image_h = 255
+
+
+pixel = image_h * image_w * 3
+
+X = []
+y = []
+
+for idx, cat in enumerate(categories):
+    img_dir_detail = img_dir + "/" + cat
+    files = glob.glob(img_dir_detail+"/*.jpg")
+
+
+    for i, f in enumerate(files):
+        try:
+            img = Image.open(f)
+            img = img.convert("RGB")
+            img = img.resize((image_w, image_h))
+            data = np.asarray(img)
+            #Y는 0 아니면 1이니까 idx값으로 넣는다.
+            X.append(data)
+            y.append(idx)
+            if i % 300 == 0:
+                print(cat, " : ", f)
+        except:
+            print(cat, str(i)+" 번째에서 에러 ")
+X = np.array(X)
+Y = np.array(y)
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1)
+
+xy = (X_train, X_test, Y_train, Y_test)
+np.save("../data/npy/sex1.npy", xy)
+
+
+# 예측할 이미지를 불러오기
+img1=[]
+for i in range(0,5):
+    try :
+        filepath='../data/image/my/me/%d.jpg'%i
+        image2=Image.open(filepath)
+        image2 = image2.convert('RGB')
+        image2 = image2.resize((255,255))
+        image_data2=asarray(image2)
+        # image_data2 = signal.medfilt2d(np.array(image_data2), kernel_size=3)
+        img1.append(image_data2)
+    except :
+        pass
+np.save("../data/npy/sex_pred.npy",arr=img1)
+
+
+# 데이터 로드
+x_train, x_test, y_train, y_test = np.load("../data/npy/sex1.npy",allow_pickle=True)
+x_pred = np.load("../data/npy/sex_pred.npy",allow_pickle=True)
+print(y_train.shape)
+print(x_train.shape)
+
+# 전처리(vgg16.trainalbe = False로 할 경우 해주고, true할거면 안해줘도 됩니다. 안해주는게 더 잘 나오는경우도 있음)
+x_train = preprocess_input(x_train)
+x_test = preprocess_input(x_test)
+x_pred = preprocess_input(x_pred)
+
+
+vgg16 = VGG16(weights='imagenet',input_shape =(255,255,3),include_top=False)
+vgg16.trainalbe = False
+model = Sequential()
+model.add(vgg16)
+model.add(GlobalAveragePooling2D())
+model.add(Flatten())
+model.add(Dense(128))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dense(64))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dense(1, activation= 'sigmoid'))
+
+
+
+# model2 = load_model('../data/modelcheckpoint/myproject.hdf5', compile=False)
+
+model.compile(loss='binary_crossentropy', optimizer=Adam(1e-5), metrics=['acc'])
+from keras.callbacks import EarlyStopping, ModelCheckpoint,ReduceLROnPlateau
+model_path = '../data/modelcheckpoint/croll.hdf5'
+checkpoint = ModelCheckpoint(filepath=model_path , monitor='val_loss', verbose=1, save_best_only=True)
+early_stopping = EarlyStopping(monitor='val_loss', patience=50)
+lr = ReduceLROnPlateau(patience=25, factor=0.5,verbose=1)
+
+history = model.fit(x_train, y_train, batch_size=16, epochs=100, validation_data=(x_test, y_test),callbacks=[early_stopping,
+checkpoint,lr])
+
+print("정확도 : %.4f" % (model.evaluate(x_test, y_test)[1]))
+result = model.predict_generator(x_pred,verbose=True)
+result[result < 0.5] =0
+result[result > 0.5] =1
+np.where(result < 0.5, '남자', '여자')
+print("남자일 확률은",result*100,"%입니다.")
+
+
+# print('acc : ', acc[-1])
+# print('val_acc : ', val_acc[-1])
+# # print('loss : ', loss[:-1])
+# # print('val_acc : ', val_loss[:-1])
+
