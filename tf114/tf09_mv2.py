622a078a8f110bc527aeaf9bfea4d720cea35d5e
lynhyul <lynhyul@naver.com>
2021-03-10 15:43:12 +0900

    c

--- tf114/tf09_mv2.py
+++ tf114/tf09_mv2.py
@@ -29,7 +29,7 @@ hypothesis = tf.matmul(x,w) + b # matmul => 행렬 전용 곱셈
 
 cost = tf.reduce_mean(tf.square(hypothesis -y))
 
-optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.00008) # larning_rate를 줄여봤다.
+optimizer = tf.train.AdamOptimizer(learning_rate = 0.9) # larning_rate를 줄여봤다.
 train = optimizer.minimize(cost)
 
 sess = tf.compat.v1.Session()
@@ -38,9 +38,8 @@ sess.run(tf.global_variables_initializer())
 for step in range(2001) :
     cost_val, hy_val, _ = sess.run([cost,hypothesis,train], 
                         feed_dict={x:x_data, y:y_data})
-    if step % 10 == 0 :
+    if step % 100 == 0 :
         print(step, "cost : ", cost_val, "\n",hy_val)
-
 
 
 
